dcd369b628b573a6c9d2fc0a9d108108b194d0d4
Kainar-Home <toleubaevkai@mail.ru>
2021-03-07 01:13:03 +0600

    applied practice style-components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,16 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+        margin: 0 auto;
+        max-width: 800px;
+`
+
+const StyledAppBlock = styled(AppBlock)`
+    background-color: grey;
+`
 
 const App = () => {
 
@@ -20,7 +30,7 @@ const App = () => {
     ];
 
     return (
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
             <div className="search-panel d-flex">
                 <SearchPanel/>
@@ -28,7 +38,7 @@ const App = () => {
             </div>
             <PostList posts={data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 
