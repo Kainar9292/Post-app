4fe1f514186c9bf5d14e2aa306fd4b51463c91a6
Kainar <toleubaevkai@mail.ru>
2021-03-09 14:56:40 +0600

    complete counter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
@@ -7,29 +7,108 @@ import PostAddForm from '../post-add-form';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { like: false,
+            data: [
+                    5,
+                    {},
+                    {label: 'Сделано на Реакт!', important: true, like: false, id: 1},
+                    {label: 'Все отлично', important: false, like: false, id: 2},
+                    {label: 'Третий пост', important: false, like: false, id: 3},
+                    {label: 'Сделано на Реакт!', important: false, like: false, id: 4}
 
-    const data = [
-        5,
-        {},
-        {label: 'Сделано на Реакт!', important: true, id:22},
-        {label: 'Все отлично', important: false, id:'wwes'},
-        {label: 'Третий пост', important: false, id:'fff'},
-        {label: 'Сделано на Реакт!', important: false, id:'www'}
+                ]
+            };
+            this.deleteItem = this.deleteItem.bind(this);
+            this.addItem = this.addItem.bind(this);
+            this.onToggleImportant = this.onToggleImportant.bind(this);
+            this.onToggleLiked = this.onToggleLiked.bind(this);
 
-    ];
+            this.maxId = 5;
+    }
 
-    return (
-        <div className="app" >
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            
+            const newArray = [...data.slice(0, index), ...data.slice(index + 1)];
+
+            return {
+                data: newArray
+            }
+            
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+
+        this.setState(({data}) => {
+            const newArray = [...data, newItem];
+            return {
+                data: newArray
+            }
+        })
+    }
+
+    onToggleImportant(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, important: !old.important};
+
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    onToggleLiked(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, like: !old.like};
+
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    render() {
+        const {data} = this.state
+        const liked = data.filter(item => item.like).length;
+        const allPosts = data.length;
+
+        return (
+            <div className="app" >
+                <AppHeader
+                    liked = {liked}
+                    allPosts={allPosts}/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList posts={this.state.data}
+                onDelete={this.deleteItem}
+                onToggleImportant={this.onToggleImportant}
+                onToggleLiked={this.onToggleLiked}/>
+                <PostAddForm
+                onAdd={this.addItem}/>
             </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </div>
-    )
+        )
+    }
 }
 
-export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const filterPosts = posts.filter(item =>
         item.label &&
@@ -13,7 +13,11 @@ const PostList = ({posts}) => {
         const {id, ...itemProps} = item;
         return (
             <li key={id} className="list-group-item">
-                <PostListItem {...itemProps}/>
+                <PostListItem 
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)}/>
             </li>
         )
     });
